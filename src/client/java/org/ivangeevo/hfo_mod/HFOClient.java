588f8cf3cc5d731be28a7acc02646ee961e995c3
ivan <dbowgaming@gmail.com>
2025-10-19 02:08:41 +0300

    bump

--- src/client/java/org/ivangeevo/hfo_mod/HFOClient.java
+++ src/client/java/org/ivangeevo/hfo_mod/HFOClient.java
@@ -1,11 +1,14 @@
 package org.ivangeevo.hfo_mod;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HFOClient implements ClientModInitializer
 {
 
     public static final String MOD_ID = "hardcore_fluid_overhaul";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
